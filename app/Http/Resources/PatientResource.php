24cbb05eefd035dc7802eb24a7f9e8365aaad682
kareem-hub <kareemehab968@gmail.com>
2023-04-07 16:13:53 +0200

    patient resource api fix

--- app/Http/Resources/PatientResource.php
+++ app/Http/Resources/PatientResource.php
@@ -16,15 +16,14 @@ class PatientResource extends JsonResource
     public function toArray($request)
     {
         $history = History::where('patient_id', $this->id)->get('properties');
-        $last_viewed_at = $this->last_viewed_at ? $this->last_viewed_at->diffForHumans() : null;
         return [
+            'id' => $this->id,
             'name' => $this->whenNotNull($this->name),
             'email' => $this->whenNotNull($this->email),
             'phone_number' => $this->whenNotNull($this->phone_number),
             'birthday' => $this->whenNotNull($this->birthday),
             'location' => $this->whenNotNull($this->location),
             'created_at' => $this->whenNotNull($this->created_at->diffForHumans()),
-            // 'last_viewed_at' => $this->whenNotNull($last_viewed_at),
             'history' => $history->isEmpty() ? 'no history yet' : $history[0]->properties
         ];
     }
